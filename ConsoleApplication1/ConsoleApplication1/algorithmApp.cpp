cbc1920f63ed6fef96995012361a82be68837ee2
ПК-ПК\ПК <timchenko2@gmail.com>
2017-03-20 08:33:26 +0200

    Fix a misspelling
     Fixed a misspelling

--- ConsoleApplication1/ConsoleApplication1/algorithmApp.cpp
+++ ConsoleApplication1/ConsoleApplication1/algorithmApp.cpp
@@ -128,14 +128,14 @@ void test_search() {
 
 	auto key = 8;
 	// key not exists in array
-	test(-1, search, Array(), key); // degerate
+	test(-1, search, Array(), key); // degenarated
 	test(-1, search, Array({ key - 1 }), key); // trivial
 	test(-1, search, Array({ key - 1, key + 1 }), key); // trivial2
 	test(-1, search, Array({ 1, 2, 3, 4, 5, 7 }), key); // general
 	test(-1, search, Array({ 9, 10, 11, 12 }), key); // general
 	test(-1, search, Array({ 4, 1, 2, 7, 10 }), key); // general
 	// key exists in array
-	// non appliable // degerate
+	// non appliable // degenarated
 	test(0, search, Array({ key }), key); // trivial
 	test(0, search, Array({ key, key + 1 }), key); // trivial2
 	test(1, search, Array({ key - 1, key }), key); // trivial2
